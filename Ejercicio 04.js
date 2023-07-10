a7255ddf26b92e328359afb0225b730c1259b75f
MateoFernandez <mateojf2000@hotmail.com>
2023-07-10 12:29:11 -0300

    se agrega la tarea

---
+++ Ejercicio 04.js
@@ -0,0 +1,44 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*4️⃣ EJERCICIO 04 4️⃣*/
+
+// ⛔️ Recuerda que debes utilizar el objeto global "Math".
+
+function elevarAlCuadrado(num) {
+   // Retorna el valor de "num" elevado al cuadrado.
+   return Math.pow(num,2);
+}
+
+function elevarAlCubo(num) {
+   // Retorna el valor de "num" elevado al cubo.
+   return Math.pow(num,3);
+}
+
+function elevar(num, exponent) {
+   // Retorna el valor de "num" elevado al exponente "exponent".
+   return Math.pow(num,exponent);
+}
+
+function redondearNumero(num) {
+   // Redondea "num" al entero más próximo y retórnalo.
+   return Math.round(num);
+}
+
+function redondearHaciaArriba(num) {
+   // Redondea "num" hacia arriba y retórnalo.
+   return Math.ceil(num);
+}
+
+function numeroRandom() {
+   // Genera un número al azar entre 0 y 1 y retórnalo.
+   return Math.random();
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   elevarAlCuadrado,
+   elevarAlCubo,
+   elevar,
+   redondearNumero,
+   redondearHaciaArriba,
+   numeroRandom,
+};
